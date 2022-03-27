0ae73f4eb11da98a3b67a39064d3743a25908e24
Joseph Lewis <joseph1601lewis.gmail.com>
2022-03-27 21:57:59 +0100

    edit

--- ODE_solver.py
+++ ODE_solver.py
@@ -1,8 +1,8 @@
 """ initial conditions """
 x0 = 1
 t0 = 0
-h = 1
-deltat_max = 2
+h = 0.25
+deltat_max = 1
 
 
 
@@ -13,35 +13,48 @@ def f(x, t):
 
 def euler_step(x, t, h):
     """ make a single Euler step """
-    x = x + f(x, t)
+    x = x + h*f(x, t)
     return x
 
 
 
-def solve_to(t0, x0, h, t1, deltat_max):
+def solve_to(t0, x0, h, deltat_max):
     """ loop through the euler function between t1 and t2"""
-    time = t0
+    t = t0
     x = f(x0, t0)
     f_array = []
     f_array.append(x)
-    space = t2 - t1
+    space = 1
     if h > deltat_max:
-        break
+        return print(' step value too high')
     else:
-        repeats = space/h
+        remainder = space%h
+        repeats = (space - remainder)/h
 
-        for i in len(repeats):
+        for i in range(int(repeats)):
             x = euler_step(x, t, h)
-            time += h
+            t += h
+            f_array.append(x)
+
+        if remainder != 0:
+            x = euler_step(x, t, remainder)
+            t += h
             f_array.append(x)
 
             
 
-    return
+    return x, f_array
 
 
 
 def solve_ode():
     return
+
+if __name__ == '__main__':
+
+    x, a = solve_to(t0, x0, h, deltat_max)
+
+    print(x)
+    print(a)
 
 
