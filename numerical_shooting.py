bdfea4ecd122e242224c7fd8723d9c74d0cf906b
Joseph Lewis <joseph1601lewis.gmail.com>
2022-04-07 13:42:31 +0100

    first attempt at solving pred-prey equations

--- numerical_shooting.py
+++ numerical_shooting.py
@@ -2,30 +2,41 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import ODE_solver as os
-
+from scipy.integrate import odeint
 
 ## Exercise 1  ##
 
 # initial conditions #
-t = np.linspace(0,100,101)
-x = 100
-y = 100
-v = [x, y]
-h = 0.0001
-deltat_max = 1
 
 
+def fpred(X, t, a=1, b=0.30, d=0.1):
 
-def fpred(t, v, t, a=1, b=0.25, d=0.1):
-
-    x = v[0]
-    y = v[1]
+    print(X)
+    print(type(X))
+    x = X[0]
+    y = X[1]
+    #x, y = v
+    #x = v[0]
+    #y = v[1]
 
     dxdt = x*(1-x)-(a*x*y)/(d+x)
-    dydt = b*y*(1-y/x)
+    dydt = b*y*(1-(y/x))
+    print([dxdt, dydt])
+    return np.array((dxdt, dydt))
 
-    return [dxdt, dydt]
+# initial conditions #
+t = np.linspace(0,1,10)
+x = 10
+y = 2
+v0 = [x, y]
+h = 0.01
+deltat_max = 1
+a=1
+b=0.20
+d=0.1
 
 
 
-a = os.solve_ode(fpred, os.rk4_step, t, v, h, deltat_max)
+#a = os.solve_ode(fpred, os.euler_step, t, v0, h)
+a = odeint(fpred, v0, t, args=(a,b,d))
+print(a)
