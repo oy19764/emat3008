7a77a0bcdbb36d27e974ce904fd5bd6ccd1319c6
Joseph Lewis <joseph1601lewis.gmail.com>
2022-04-28 15:53:10 +0100

    natural parameters works for hopf

--- continuation.py
+++ continuation.py
@@ -17,33 +17,36 @@ delta_n = 0.001 # step size
 
 
 
-def natural_parameters(f, cmax, cmin, delta_n, u0):
+def natural_parameters(f, cmax, cmin, delta_n, u0, ODE):
 
     n = (cmax - cmin)/delta_n
     k = np.linspace(cmin, cmax, math.floor(abs(n)))
+    
 
-    discretisation = lambda u, alpha, f: f(u, alpha)
+    if ODE == False:
+        discretisation = lambda u, alpha, f: f(u, alpha)
+    else: 
+        discretisation = lambda u, alpha, f: shooting.shoot(f, u, alpha)
 
-
+    print(u0)
     sol = []
-    sol.append(fsolve(lambda U: discretisation(U, cmin, f), u0))
-
+    sol.append(fsolve(discretisation, u0, args=(cmin, f)))      #(lambda U: discretisation(U, cmin, f), u0))
+    
     for a in range(1, len(k)):
+        sol.append(fsolve(discretisation, sol[-1], args=(k[a], f)))    #lambda U: discretisation(U, k[a], f), sol[-1]))
+    
 
-        
-        sol.append(fsolve(lambda U: discretisation(U, k[a], f), sol[-1]))
-    sol = np.concatenate(sol)
     return sol, k
 
-"""
-sol, alpha = natural_parameters(cubic, cmax, cmin, delta_n, 2)
 
-plt.plot(alpha, sol)
-plt.show()
-"""
+#sol, alpha = natural_parameters(cubic, cmax, cmin, delta_n, 2, ODE = False)
+
+#plt.plot(alpha, sol)
+#plt.show()
+#print(sol)
 
 
-def hopf(u, beta):
+def hopf(u, t, beta):
 
     u1, u2 = u
 
@@ -54,20 +57,20 @@ def hopf(u, beta):
 
 cmin = 0
 cmax = 2
-delta_n = 0.2 # step size
+delta_n = 0.01 # step size
 
-sol, alpha = natural_parameters(hopf, cmax, cmin, delta_n, (0.5,0.4))
+sol, alpha = natural_parameters(hopf, cmax, cmin, delta_n, (1.4, 0, 6.3), ODE=True)
 print(sol)
 
-#plt.plot(alpha, sol[0,:])
-#plt.plot(alpha,sol[1,:])
+u1 = [sol[0][0]]
+u2 = [sol[0][1]]
+for i in range(1,len(alpha)):
+    u1.append(sol[i][0])
+    u2.append(sol[i][1])
 #plt.show()
-
-
-
-
-
-
+plt.plot(alpha, u1)
+plt.plot(alpha, u2)
+plt.show()
 
 
 def hopf_mod(u, t, beta):
