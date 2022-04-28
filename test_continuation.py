0d2dda50a8bde0c95657e3f7d14b95680454e67b
Joseph Lewis <joseph1601lewis.gmail.com>
2022-04-28 17:36:43 +0100

    moved natural parameter function to other script

--- test_continuation.py
+++ test_continuation.py
@@ -1,11 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from pyparsing import alphas
 import ODE_solver as os
 from scipy.optimize import fsolve
-from scipy.integrate import odeint
-import shooting
-import math
+from continuation import natural_parameters
 
 def cubic(x, c):
 
@@ -16,34 +13,11 @@ cmin = -2
 delta_n = 0.001 # step size
 
 
-
-def natural_parameters(f, cmax, cmin, delta_n, u0, ODE):
-
-    n = (cmax - cmin)/delta_n
-    k = np.linspace(cmin, cmax, math.floor(abs(n)))
-    
-
-    if ODE == False:
-        discretisation = lambda u, alpha, f: f(u, alpha)
-    else: 
-        discretisation = lambda u, alpha, f: shooting.shoot(f, u, alpha)
-
-    print(u0)
-    sol = []
-    sol.append(fsolve(discretisation, u0, args=(cmin, f)))      #(lambda U: discretisation(U, cmin, f), u0))
-    
-    for a in range(1, len(k)):
-        sol.append(fsolve(discretisation, sol[-1], args=(k[a], f)))    #lambda U: discretisation(U, k[a], f), sol[-1]))
-    
-
-    return sol, k
-
-
 #sol, alpha = natural_parameters(cubic, cmax, cmin, delta_n, 2, ODE = False)
 
 #plt.plot(alpha, sol)
 #plt.show()
-#print(sol)
+
 
 
 def hopf(u, t, beta):
@@ -59,17 +33,45 @@ cmin = 0
 cmax = 2
 delta_n = 0.01 # step size
 
-sol, alpha = natural_parameters(hopf, cmax, cmin, delta_n, (1.4, 0, 6.3), ODE=True)
-print(sol)
+sol, alpha = natural_parameters(hopf, cmax, cmin, delta_n, (1.4, 0, 6), ODE=True)
+#print(sol)
 
 u1 = [sol[0][0]]
 u2 = [sol[0][1]]
+T = [sol[0][2]]
 for i in range(1,len(alpha)):
     u1.append(sol[i][0])
     u2.append(sol[i][1])
-#plt.show()
+    T.append(sol[i][2])
+
 plt.plot(alpha, u1)
 plt.plot(alpha, u2)
+plt.show()
+
+def true_hopf(t, beta, theta = 0.0):
+
+    u1 = np.sqrt(beta) * np.cos(t + theta)
+    u2 = np.sqrt(beta) * np.sin(t + theta)
+
+    return np.array([u1, u2])
+
+
+
+
+true_roots = []
+for a, t in list(zip(alpha, T)):
+    
+    true_roots.append(true_hopf(0, a, theta = t))
+
+print(true_roots)
+root_u1 = []
+root_u2 = []
+for i in range(0,len(alpha)):
+    root_u1.append(true_roots[i][0])
+    root_u2.append(true_roots[i][1])
+
+plt.plot(alpha, root_u1)
+plt.plot(alpha, root_u2)
 plt.show()
 
 
