6252da2486bcacaaac306e50337e03478d710cba
Joseph Lewis <joseph1601lewis.gmail.com>
2022-04-29 13:35:36 +0100

    small edits and testing

--- test_continuation.py
+++ test_continuation.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import ODE_solver as os
 from scipy.optimize import fsolve
-from continuation import natural_parameters
+from continuation import natural_parameters, pseudo_arclength
 
 def cubic(x, c):
 
@@ -13,10 +13,10 @@ cmin = -2
 delta_n = 0.001 # step size
 
 
-#sol, alpha = natural_parameters(cubic, cmax, cmin, delta_n, 2, ODE = False)
+sol, alpha = natural_parameters(cubic, cmax, cmin, delta_n, 2, ODE = False)
 
-#plt.plot(alpha, sol)
-#plt.show()
+plt.plot(alpha, sol)
+plt.show()
 
 
 
@@ -31,22 +31,22 @@ def hopf(u, t, beta):
 
 cmin = 0
 cmax = 2
-delta_n = 0.01 # step size
+delta_n = 0.1 # step size
 
-sol, alpha = natural_parameters(hopf, cmax, cmin, delta_n, (1.4, 0, 6), ODE=True)
-#print(sol)
+sol, alpha = natural_parameters(hopf, cmax, cmin, delta_n, (1.4, 0, 6.3), ODE=True)
+print(sol)
 
-u1 = [sol[0][0]]
-u2 = [sol[0][1]]
-T = [sol[0][2]]
-for i in range(1,len(alpha)):
-    u1.append(sol[i][0])
-    u2.append(sol[i][1])
-    T.append(sol[i][2])
+#u1 = [sol[0][0]]
+#u2 = [sol[0][1]]
+#T = [sol[0][2]]
+#for i in range(1,len(alpha)):
+#    u1.append(sol[i][0])
+#    u2.append(sol[i][1])
+#    T.append(sol[i][2])
 
-plt.plot(alpha, u1)
-plt.plot(alpha, u2)
-plt.show()
+#plt.plot(alpha, u1)
+#plt.plot(alpha, u2)
+#plt.show()
 
 def true_hopf(t, beta, theta = 0.0):
 
@@ -58,21 +58,21 @@ def true_hopf(t, beta, theta = 0.0):
 
 
 
-true_roots = []
-for a, t in list(zip(alpha, T)):
-    
-    true_roots.append(true_hopf(0, a, theta = t))
+#true_roots = []
+#for a, t in list(zip(alpha, T)):
+#    
+#    true_roots.append(true_hopf(0, a, theta = t))#
 
-print(true_roots)
-root_u1 = []
-root_u2 = []
-for i in range(0,len(alpha)):
-    root_u1.append(true_roots[i][0])
-    root_u2.append(true_roots[i][1])
+#print(true_roots)
+#root_u1 = []
+#root_u2 = []
+#for i in range(0,len(alpha)):
+#    root_u1.append(true_roots[i][0])
+#    root_u2.append(true_roots[i][1])
 
-plt.plot(alpha, root_u1)
-plt.plot(alpha, root_u2)
-plt.show()
+#plt.plot(alpha, root_u1)
+#plt.plot(alpha, root_u2)
+#plt.show()
 
 
 def hopf_mod(u, t, beta):
@@ -83,3 +83,6 @@ def hopf_mod(u, t, beta):
     du2dt = u1 + beta*u2 + u2*(u1**2 + u2**2) - u2*(u1**2 + u2**2)**2
 
     return np.array((du1dt, du2dt))
+
+
+#print(pseudo_arclength(hopf, cmax, cmin, delta_n, (1.4,0,6), ODE = True))
