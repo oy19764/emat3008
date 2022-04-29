87a1bf02a3b5c04392c4b45b4a8f04e8bcdfac79
Joseph Lewis <joseph1601lewis.gmail.com>
2022-04-29 13:35:05 +0100

    set out plan for pseudo arc length function

--- continuation.py
+++ continuation.py
@@ -25,4 +25,31 @@ def natural_parameters(f, cmax, cmin, delta_n, u0, ODE):
     return sol, k
 
 
-#def pseudo_arclength():
+def pseudo_arclength(f, cmax, cmin, delta_n, u0, ODE):
+
+    if ODE == False:
+        discretisation = lambda u, alpha, f: f(u, alpha)
+    else: 
+        discretisation = lambda u, alpha, f: shooting.shoot(f, u, alpha)
+
+    # two input parameters: v0 and v1  -  v0 = [alpha0, u0], v1 = [alpha1, u1]
+    n = (cmax - cmin)/delta_n
+    k = np.linspace(cmin, cmax, math.floor(abs(n)))
+    diff = 0.01 * np.sign(cmax-cmin)
+    alpha0 = k[0]
+    alpha1 = alpha0 + diff
+    v0 = np.append(fsolve(discretisation, u0, args=(alpha0, f)))        # v0 = [alpha0, u0]
+    v1 = np.append(fsolve(discretisation, v0[:-1], args=(alpha1, f)))   # v1 = [alpha1, u1]
+
+    # generate secant: change = vi - vi-1
+
+
+
+    # predict solution: v~i+1 = vi +change (approximation)
+
+    # solve augmented root finding problem: (vi+1 - v~i+1)*change = 0
+
+    # repeat until all parameters covered
+
+
+    return n, k
