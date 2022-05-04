ba1de378f304df7edd8056267f1466f954947271
Joseph Lewis <joseph1601lewis.gmail.com>
2022-05-04 18:29:56 +0100

    added argument to specify the phase condition

--- shooting.py
+++ shooting.py
@@ -4,7 +4,7 @@ from scipy.optimize import fsolve
 
 
 
-def G(ODE, u0, T, *args):
+def G(ODE, u0, T, pc, *args):
     """
     Returns the difference between the initial conditions and the solution to the ODE,
     and finds the phase condition.
@@ -12,41 +12,43 @@ def G(ODE, u0, T, *args):
                 ODE (function):         The ODE of which the limit cycle is to be found
                 u0 (tuple):             Estimate of the startig conditions
                 T (float):              Esstimate of the time period
+                pc                      Phase condition to be used
                 args:                   Any additional arguments required for the ODE
         """    
 
     sol = os.solve_ode(ODE, os.rk4_step, [0, T], u0, 0.001, *args)
     g1 = u0 - sol[-1]
-    g2 = ODE(u0, 0,*args)[0] # phase condition
+    g2 = ODE(u0, 0,*args)[pc] # phase condition
     gsol = np.append(g1, g2)
     return gsol
 
 
-def shoot(ODE, U, *args):
+def shoot(ODE, U, pc, *args):
 
     u0 = U[:-1]
     T = U[-1]
-    shot = G(ODE, u0, T, *args)
+    shot = G(ODE, u0, T, pc, *args)
 
     return shot
 
 
-
-
-def limit_cycle(ODE ,U0, *args):
+def limit_cycle(ODE ,U0, pc,*args):
     """
     Solves the shooting problem to find the roots to G, hence u0 ant T which are
     the conditions to make a limit cycle
         Parameters:
                 ODE (function):         The ODE of which the limit cycle is to be found
                 U0 (tuple):             The estimated initial conditions and time period of the ODE limit cycle
+                pc(int)                 The phase condition specified to be used
                 args:                   Any additional arguments required for the ODE
                 
         Returns:
                 u0 (tuple):             Initial conditions of the limit cycle
                 T (float):              Time period of the limit cycle
     """
-    sol = fsolve(lambda U: shoot(ODE, U, *args), U0)
-    #u0 = sol[:-1]
-    #T = sol[-1]
-    return sol             #u0, T
+    sol = fsolve(lambda U: shoot(ODE, U, pc, *args), U0)
+   
+    return sol          
+
+
+    
