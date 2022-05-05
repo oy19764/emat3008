bf4ca4cf67eeb762a525e9543fa4b39b4445a9aa
Joseph Lewis <joseph1601lewis.gmail.com>
2022-05-05 22:13:29 +0100

    neumann boundary condition added

--- pde.py
+++ pde.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 from math import pi
 from scipy.sparse import diags
 from scipy.sparse.linalg import spsolve
+from scipy.sparse import csr_matrix
 
 
 def solve_pde(u_I, L, T, mt, mx, kappa, pj, qj ,boundary, method):
@@ -38,114 +39,130 @@ def solve_pde(u_I, L, T, mt, mx, kappa, pj, qj ,boundary, method):
     print("deltat=",deltat)
     print("lambda=",lmbda)
 
-    # Set up the solution variables
-    #u_j = np.zeros(x.size)        # u at current time step
-    #u_jp1 = np.zeros(x.size)      # u at next time step
-
-
 
     def boundary_type(boundary, pj, qj):
         if boundary == 'dirilichet':
             matrix_dim = mx - 1
             print(matrix_dim)
             u_j = np.zeros(x.size)
-            u_j[0] = pj
-            u_j[-1] = qj
+            #u_j[0] = pj
+            #u_j[-1] = qj
+            for i in range(0,len(u_j)):
+                u_j[i] = u_I(x[i])
+
+            return matrix_dim, u_j
+
+
+        if boundary == 'neumann':
+            matrix_dim = mx + 1
+            u_j = np.zeros(x.size)
+            
             for i in range(0,len(u_j)):
                 u_j[i] = u_I(x[i])
             return matrix_dim, u_j
 
+    # define additive vector
+    def additive_vector(boundary):
+        if boundary == 'dirilichet':
+            return np.zeros(mx-1)
+        if boundary == 'neumann':
+            return np.zeros(mx+1)
+        
 
-        if boundary == 'neumman':
-            matrix_dim = mx + 1
-            u_j = np.zeros(x.size)
-            for i in range(0,len(u_j)):
-                
-                return
-
-    # define grid
-
-
-    def tri_matrix(method, matrix_dim):
+    def tri_matrix(method, matrix_dim, boundary):
 
         # Set tridiagonal matrix 
         # forward euler
         if method == 'forward':
             diag = [[lmbda] * (matrix_dim-1), [1 - 2*lmbda] * matrix_dim , [lmbda] * (matrix_dim-1)]
             tridiag = diags(diag, offsets = [-1,0,1], format = 'csc')
-            return tridiag, None
+            if boundary == 'dirilichet':
+                return tridiag, None
+            if boundary == 'neumann':
+                tridiag = tridiag.toarray()
+                tridiag[0,1] *= 2
+                tridiag[-1,-2] *= 2   
+                return csr_matrix(tridiag), None
         
         #backwards euler
         if method == 'backward':
             diag = [[-lmbda] * (matrix_dim-1), [1 + 2*lmbda] * matrix_dim , [-lmbda] * (matrix_dim-1)]
             tridiag = diags(diag, offsets = [-1,0,1], format = 'csc')
-            return tridiag, None
+            if boundary == 'dirilichet':
+                return tridiag, None
+            if boundary == 'neumann':
+                tridiag = tridiag.toarray()
+                tridiag[0,1] = tridiag[0,1]*2
+                tridiag[-1,-2] = tridiag[-1,-2]*2
+                return csr_matrix(tridiag), None
         
         if method == 'crank':
             diag = [[-lmbda/2] * (matrix_dim-1), [1 + lmbda] * matrix_dim , [-lmbda/2] * (matrix_dim-1)]
             diag2 = [[lmbda/2] * (matrix_dim-1), [1 - lmbda] * matrix_dim , [lmbda/2] * (matrix_dim-1)]
             tridiag = diags(diag, offsets = [-1,0,1], format = 'csc')
             tridiag2 = diags(diag2, offsets = [-1,0,1], format = 'csc')
-            return tridiag, tridiag2
-
-
-    # def additive_V(pj, qj):
-    #     pj = 0
-    #     qj = 0
-    #     if method == 'dirilichet':
-    #         aV = np.zeros(mx-1)
-    #         aV[0] = pj
-    #         aV[-1] = qj    
-    #     return aV
-
-    
-
-    # # Set initial condition
-    # for i in range(0, mx+1):
-    #     u_j[i] = u_I(x[i])
-
-
-    
+            if boundary == 'dirilichet':
+                return tridiag, tridiag2
+            if boundary == 'neumann':
+                tridiag = tridiag.toarray()
+                tridiag2 = tridiag2.toarray()
+                tridiag[0,1] = tridiag[0,1]*2
+                tridiag[-1,-2] = tridiag[-1,-2]*2
+                tridiag2[0,1] = tridiag2[0,1]*2
+                tridiag2[-1,-2] = tridiag2[-1,-2]*2
+                return csr_matrix(tridiag), csr_matrix(tridiag2)
+            
+                
 
     matrix_dim, u_j = boundary_type(boundary, pj, qj)
-    u_jp1 = np.zeros(x.size)
+    u_jp1 = np.zeros(mx+1)
     
    
-   
     # get diagonals corresponding to the method
-    diag1, diag2 = tri_matrix(method, matrix_dim)
-
-
-    #setup solution grid
-    #grid = np.zeros((self.mt, self.mx + 1))
+    diag1, diag2 = tri_matrix(method, matrix_dim, boundary)
 
 
     # setup additive vector
-    aV = np.zeros(mx-1)
-    aV[0] = pj
-    aV[-1] = qj
+    aV = additive_vector(boundary)
+    
 
     for i in range(0,mt-1):
         # forwad euler matrix calc
-        if method == 'forward':
-            u_jp1[1:-1] =  diag1.dot(u_j[1:-1]) + aV*lmbda
+        if boundary == 'dirilichet':
+            aV[0], aV[-1] = pj, qj
+            if method == 'forward':
+                u_jp1[1:-1] = diag1.dot(u_j[1:-1]) + aV * lmbda
+
+            # backwards euler matrix calc
+            if method == 'backward':
+                u_jp1[1:-1] = spsolve(diag1, u_j[1:-1]) + aV * lmbda
+
+            # crank-nicholson matrix calc
+            if method == 'crank': #that soulja boy
+                u_jp1[1:-1] = spsolve(diag1, diag2.dot(u_j[1:-1])) + aV * lmbda
+
+            # add boundary conditions
+            u_jp1[0] = aV[0]
+            u_jp1[-1] = aV[-1]
+            #initialise u_j for the next time step
+            u_j[:] = u_jp1[:]
+
+        if boundary == 'neumann':
+            aV[0], aV[-1] = -pj, qj
+            if method == 'forward':
+                u_jp1 = diag1.dot(u_j) + 2 * lmbda * deltax * aV
+            # backwards euler matrix calc
+            if method == 'backward':
+                u_jp1 = spsolve(diag1, u_j) + aV * lmbda
+            # crank-nicholson matrix calc
+            if method == 'crank': #that soulja boy
+                u_jp1 = spsolve(diag1, diag2.dot(u_j)) + aV * lmbda
+                
+            u_j[:] = u_jp1[:]
 
 
-        # backwards euler matrix calc
-        if method == 'backward':
-            u_jp1[1:-1] = spsolve(diag1, u_j[1:-1])
-
-        # crank-nicholson matrix calc
-        if method == 'crank':
-            u_jp1[1:-1] = spsolve(diag1, diag2.dot(u_j[1:-1]))
-
-        #initialise u_j for the next time step
-        u_j[:] = u_jp1[:]
 
     return x, u_j
-
-
-
 
 
 
@@ -176,11 +193,11 @@ mx = 30     # number of gridpoints in space
 mt = 1000   # number of gridpoints in time
 
 # boundary values
-pj = 0
-qj = 0
+p = 0
+q = 0
 
 
-x, u_j = solve_pde(u_I, L, T, mt, mx, kappa, pj, qj, boundary = 'dirilichet', method = 'forward')
+x, u_j = solve_pde(u_I, L, T, mt, mx, kappa, p, q, boundary = 'neumann', method = 'crank')
 
 # plot the final result and exact solution
 plt.plot(x,u_j,'ro',label='num')
