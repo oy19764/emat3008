b202673afcafc97c04a6d9218426705da9d3ee9c
Joseph Lewis <joseph1601lewis.gmail.com>
2022-05-07 16:13:18 +0100

    shortened code

--- shooting.py
+++ shooting.py
@@ -5,32 +5,40 @@ from ODE_solver import test_inputs
 
 
 
-def G(ODE, u0, T, pc, *args):
+def G(ODE, U, pc, *args):
     """
     Returns the difference between the initial conditions and the solution to the ODE,
     and finds the phase condition.
         Parameters:
                 ODE (function):         The ODE of which the limit cycle is to be found
-                u0 (tuple):             Estimate of the startig conditions
-                T (float):              Esstimate of the time period
-                pc(int)                      Phase condition to be used
+                U(tuple):               Tuple containing the estimate of the starting conditions and time period
+                pc(function)            The phase condition function specified to be used
                 args:                   Any additional arguments required for the ODE
         """    
+    # unpack U
+    u0 = U[:-1]
+    T = U[-1]
 
-    sol = os.solve_ode(ODE, os.rk4_step, [0, T], u0, 0.001, *args)
+    # check shape
+    if len(u0) > 1:
+        system = True
+    else:
+        system = False
+
+    sol = os.solve_ode(ODE, os.rk4_step, [0, T], u0, 0.001, system,*args)
     g1 = u0 - sol[-1]
-    g2 = ODE(u0, 0,*args)[pc] # phase condition
+    g2 = pc(u0, *args)   #ODE(u0, 0,*args)[pc] # phase condition
     gsol = np.append(g1, g2)
     return gsol
 
 
-def shoot(ODE, U, pc, *args):
+# def shoot(ODE, U, pc, *args):
 
-    u0 = U[:-1]
-    T = U[-1]
-    shot = G(ODE, u0, T, pc, *args)
+#     u0 = U[:-1]
+#     T = U[-1]
+#     shot = G(ODE, u0, T, pc, *args)
 
-    return shot
+#     return shot
 
 
 def limit_cycle(ODE ,U0, pc,*args):
@@ -40,7 +48,7 @@ def limit_cycle(ODE ,U0, pc,*args):
         Parameters:
                 ODE (function):         The ODE of which the limit cycle is to be found
                 U0 (tuple):             The estimated initial conditions and time period of the ODE limit cycle
-                pc(int)                 The phase condition specified to be used
+                pc(function)            The phase condition function specified to be used
                 args:                   Any additional arguments required for the ODE
                 
         Returns:
@@ -53,10 +61,10 @@ def limit_cycle(ODE ,U0, pc,*args):
     # test U0 is a tuple:
     test_inputs(U0,'U0', 'test_tuple')
     # test pc is an integer
-    test_inputs(pc,'pc','test_int_or_float')
+    test_inputs(pc,'pc','test_function')
     
 
-    sol = fsolve(lambda U: shoot(ODE, U, pc, *args), U0)
+    sol = fsolve(lambda U: G(ODE, U, pc, *args), U0)
    
     return sol          
 
