5c193fd50bf05f9c656ede2852ecc8fccc7b306c
Joseph Lewis <joseph1601lewis.gmail.com>
2022-05-08 00:32:38 +0100

    parameter change

--- continuation.py
+++ continuation.py
@@ -1,57 +1,48 @@
 import numpy as np
 import ODE_solver as os
 from scipy.optimize import fsolve
-import shooting
 import math
 from tqdm import tqdm
 
-def natural_parameters(f, cmax, cmin, delta_n, u0, discretisation, solver=fsolve, pc=None):
+def natural_parameters(f, par_range, delta_n, u0, discretisation, solver=fsolve, pc=None):
+    """
+    Natural Parameters continuation method. Finds the solution to the ode function for
+    each parameter in the parameter range"""
+    
 
+    param_n = (par_range[1] - par_range[0])/delta_n     # number of parameters
+    param_list = np.linspace(par_range[0], par_range[1], math.floor(abs(param_n)))  #parameter list
 
     def get_sol(f, alpha, discretisation, u0, pc):
 
         if pc == None:
-            #arg = alpha
             return solver(discretisation(f), u0, args=alpha)
         else:
-            #arg = (pc, alpha)
-            return(discretisation(f, u0, pc, alpha))
+            return solver(discretisation(f), u0, args=(pc, alpha))
 
-
-
-        #return solver(discretisation(f), u0, args=arg)
-
-        # fsolve(lambda U: shoot(ODE, U, pc, *args), U0)
-
-    param_n = (cmax - cmin)/delta_n     # number of parameters
-    param_list = np.linspace(cmin, cmax, math.floor(abs(param_n)))  #parameter list
-    
 
     sol = []  
-    # if ODE == False:   
-    #     sol.append(fsolve(lambda u: f(u, cmin), u0)) # discretisation for non-ODE
-    # else:
-    #     sol.append(shooting.limit_cycle(f, u0, pc, cmin)) # discretisation for ODE
-
-    sol.append(get_sol(f, cmin, discretisation, u0, pc))
+    sol.append(get_sol(f, param_list[0], discretisation, u0, pc))
 
     for a in tqdm(range(1, len(param_list))):
-        sol.append(get_sol(f, param_list[a], discretisation, tuple(sol[-1]), pc))
+        sol.append(get_sol(f, param_list[a], discretisation, np.round(tuple(sol[-1]), 3), pc))
 
-        # if ODE == False:   
-        #     sol.append(fsolve(lambda u: f(u, param_list[a]), sol[-1]))
-        # else:
-        #     sol.append(shooting.limit_cycle(f, sol[-1], pc, param_list[a]))
           
     return sol, param_list
 
 
-def pseudo_arclength(f, cmax, cmin, delta_n, u0, ODE, pc):
 
-    if ODE == False:
-        discretisation = lambda u, alpha, f: f(u, alpha)
-    else: 
-        discretisation = lambda u, alpha, f: shooting.shoot(f, u, alpha, pc)
+
+
+def pseudo_arclength(f, cmax, cmin, delta_n, u0, ODE, discretisation, solver=fsolve, pc=None):
+
+    def get_sol(f, alpha, discretisation, u0, pc):
+
+        if pc == None:
+            return solver(discretisation(f), u0, args=alpha)
+        else:
+            return(discretisation(f, u0, pc, alpha))
+
 
     # two input parameters: v0 and v1  -  v0 = [alpha0, u0], v1 = [alpha1, u1]
     param_n = (cmax - cmin)/delta_n
