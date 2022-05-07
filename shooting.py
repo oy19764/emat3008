3430ade0d3123da374d0e1fd1de2cf77d55a52a1
Joseph Lewis <joseph1601lewis.gmail.com>
2022-05-08 00:30:08 +0100

    cleaned shooting functio to work for continuation

--- shooting.py
+++ shooting.py
@@ -5,40 +5,35 @@ from ODE_solver import test_inputs
 
 
 
-def G(ODE, U, pc, *args):
-    """
-    Returns the difference between the initial conditions and the solution to the ODE,
-    and finds the phase condition.
-        Parameters:
-                ODE (function):         The ODE of which the limit cycle is to be found
-                U(tuple):               Tuple containing the estimate of the starting conditions and time period
-                pc(function)            The phase condition function specified to be used
-                args:                   Any additional arguments required for the ODE
-        """    
-    # unpack U
-    u0 = U[:-1]
-    T = U[-1]
+def shoot(ODE):
+        
+        def G(U, pc, *args):
+            """
+            Returns the difference between the initial conditions and the solution to the ODE,
+            and finds the phase condition.
+                Parameters:
+                        U(tuple):               Tuple containing the estimate of the starting conditions and time period
+                        pc(function)            The phase condition function specified to be used
+                        args:                   Any additional arguments required for the ODE
+            """    
+            # unpack U
+            u0 = U[:-1]
+            T = U[-1]
 
-    # check shape
-    if len(u0) > 1:
-        system = True
-    else:
-        system = False
+            # check shape
+            if len(u0) > 1:
+                system = True
+            else:
+                system = False
 
-    sol = os.solve_ode(ODE, os.rk4_step, [0, T], u0, 0.001, system,*args)
-    g1 = u0 - sol[-1]
-    g2 = pc(u0, *args)   #ODE(u0, 0,*args)[pc] # phase condition
-    gsol = np.append(g1, g2)
-    return gsol
+            sol = os.solve_ode(ODE, os.rk4_step, [0, T], u0, 0.01, system,*args)
+            g1 = u0 - sol[-1]
+            g2 = pc(u0, *args)   #ODE(u0, 0,*args)[pc] # phase condition
+            gsol = np.append(g1, g2)
 
+            return gsol
 
-# def shoot(ODE, U, pc, *args):
-
-#     u0 = U[:-1]
-#     T = U[-1]
-#     shot = G(ODE, u0, T, pc, *args)
-
-#     return shot
+        return G
 
 
 def limit_cycle(ODE ,U0, pc,*args):
@@ -60,12 +55,12 @@ def limit_cycle(ODE ,U0, pc,*args):
     test_inputs(ODE, 'ODE', 'test_function')
     # test U0 is a tuple:
     test_inputs(U0,'U0', 'test_tuple')
-    # test pc is an integer
+    # test pc is a function
     test_inputs(pc,'pc','test_function')
     
 
-    sol = fsolve(lambda U: G(ODE, U, pc, *args), U0)
-   
+    
+    sol = fsolve(shoot(ODE),U0,args=(pc,*args))        
     return sol          
 
 
