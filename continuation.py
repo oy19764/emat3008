cf1c2baf3c142f78dfb1c9d525cd2c6d4fb0bba4
Joseph Lewis <joseph1601lewis.gmail.com>
2022-05-08 17:06:42 +0100

    added input/output tests

--- continuation.py
+++ continuation.py
@@ -3,11 +3,50 @@ import ODE_solver as os
 from scipy.optimize import fsolve
 import math
 from tqdm import tqdm
+from ODE_solver import test_inputs
 
 def natural_parameters(f, par_range, delta_n, u0, discretisation, solver=fsolve, pc=None):
     """
     Natural Parameters continuation method. Finds the solution to the ode function for
-    each parameter in the parameter range"""
+    each parameter in the parameter range
+        Parameters:
+            f (function):               ODE for which numerical continuation is being performed
+            par_range (tuple):          parameter range numerical continuation is solving for
+            delta_n (float):            increments between parameter values
+            u0 (tuple):                 tuple containing estimate for inital values and time period
+            discretisation(function):   method
+            solver:                     rootfinding method to be used, defaults to fsolve
+            pc(function):               phase condition function to be used if solving system of ODE's           """
+    
+
+    # test parameters:
+    # test par_range is a tuple
+    test_inputs(par_range, 'par_range', 'test_tuple')
+    # test delta_n is float
+    test_inputs(delta_n, 'delta_n', 'test_int_or_float')
+    # test u0 is a tuple
+    test_inputs(u0, 'u0', 'test_array')
+    # test discretisation is a function
+    test_inputs(discretisation, 'discretisation', 'test_function')
+    # test f is a function
+    if callable(f):
+        # test f returns valid output
+        t_test = u0[-1]
+        test_output = f(u0[:-1], t_test, par_range[0])
+        # test valid output type
+        if isinstance(test_output, (int, float, np.int_, np.float_, list, np.ndarray)):
+            # test valid output size
+            if np.array(test_output).shape == np.array(u0[:-1]).shape:
+               pass
+            else:
+                raise ValueError(f"Invalid output shape from ODE function {f},\n"
+                                "u0[:-1] and {f} output should have the same shape")
+        else:
+            raise TypeError(f"Output from ODE function is a {type(test_output)},\n"
+            "output should be an integer, float, list or array")
+    else: 
+        raise TypeError(f"{f} is not a valid input type. \n" 
+                            "Please input a function")
     
 
     param_n = (par_range[1] - par_range[0])/delta_n     # number of parameters
@@ -26,7 +65,6 @@ def natural_parameters(f, par_range, delta_n, u0, discretisation, solver=fsolve,
 
     for a in tqdm(range(1, len(param_list))):
         sol.append(get_sol(f, param_list[a], discretisation, np.round(tuple(sol[-1]), 3), pc))
-
           
     return sol, param_list
 
