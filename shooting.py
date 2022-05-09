9770fd48e720c27d365750f152dd18096986026a
Joseph Lewis <joseph1601lewis.gmail.com>
2022-05-09 18:41:50 +0100

    clean up and formatting

--- shooting.py
+++ shooting.py
@@ -33,7 +33,7 @@ def shoot(ODE):
         else:
             system = False
 
-        sol = os.solve_ode(ODE, os.rk4_step, [0, T], u0, 0.01, system,*args)
+        sol = os.solve_ode(ODE, [0, T], u0, 0.01, system, os.rk4_step,*args)
         g1 = u0 - sol[-1]
         g2 = pc(u0, *args)   
         gsol = np.append(g1, g2)
@@ -63,7 +63,7 @@ def limit_cycle(ODE ,U0, pc,*args):
     # test pc is a function
     test_inputs(pc,'pc','test_function')
     # test ODE is an ODE:
-    test_ode(f, U0[:-1], *args)
+    test_ode(ODE, U0[:-1], *args)
 
 
     sol = fsolve(shoot(ODE),U0,args=(pc,*args))        
@@ -90,18 +90,21 @@ if __name__ == '__main__':
         
         return np.array((dxdt, dydt))
 
+
     #solving using b = 0.36 > 0.26
     a = 1
     b = 0.36
     d = 0.1
 
+
     # initial conditions
     x0 = [0.35, 0.35]
     t = np.linspace(0,100,3001)
     h = 0.001
-    sol = os.solve_ode(f, os.rk4_step, t, x0, h, True, a, b, d)
+    sol = os.solve_ode(f, t, x0, h, True, os.rk4_step, a, b, d)
     x = sol[:,0]
     y = sol[:,1]
+
 
     # plot time series
     plt.subplot(2,2,1)
@@ -110,6 +113,7 @@ if __name__ == '__main__':
     plt.ylabel('locta volterra values')
     plt.xlabel('t')
     plt.title('locta volterra time series for b > 0.26')
+
     # plot y against x
     plt.subplot(2,2,2)
     plt.plot(x, y)
@@ -117,18 +121,22 @@ if __name__ == '__main__':
     plt.xlabel('x')
     plt.title('locta volterra phase portrait for b > 0.26')
 
+
     # Solving using b = 0.16 < 0.26
     b = 0.16
 
-    sol = os.solve_ode(f, os.rk4_step, t, x0, h, True, a, b, d)
+
+    sol = os.solve_ode(f, t, x0, h, True, os.rk4_step, a, b, d)
     x = sol[:,0]
     y = sol[:,1]
+
     # plot time series
     plt.subplot(2,2,3)
     plt.plot(t, x)
     plt.plot(t, y)
     plt.ylabel('locta volterra values')
     plt.xlabel('t')
+
     plt.title('locta volterra time series for b < 0.26')
     # plot y against x
     plt.subplot(2,2,4)
@@ -138,6 +146,7 @@ if __name__ == '__main__':
     plt.title('locta volterra phase portrait for b < 0.26')
     plt.show()
 
+
     """
         The figures show different outcomes for the predator-prey equations depending on the value of b.
     When b > 0.26, the fluctuation in population seems to flatten out. This is reinforced by the 
@@ -146,7 +155,10 @@ if __name__ == '__main__':
     of y against x shows the development of a circle, indication the formation of period orbits.
     """
 
+
+
     ## Isolate a periodic orbit ##
+
     def find_orbit(f, t, x, y, h, *args):
         period_list = []
         intersect = []
@@ -165,7 +177,7 @@ if __name__ == '__main__':
         start = intersect[-2]
     
         t_orb = np.linspace(period_list[-2],period_list[-1],200)
-        orbitsol = os.solve_ode(f, os.rk4_step, t_orb, start, h, True,*args)
+        orbitsol = os.solve_ode(f, t_orb, start, h, True, os.rk4_step,*args)
 
         orbit_x = orbitsol[:,0]
         orbit_y = orbitsol[:,1]
@@ -180,38 +192,44 @@ if __name__ == '__main__':
         return period, orbit_x, orbit_y, orbitsol
 
 
+
     # isolated period orbit
     period, orbit_x, orbit_y, manual_orbitsol = find_orbit(f, t, x, y, h, a, b, d)
+
     #period
     print('The period of the orbit is:  {}'.format(period))
 
 
-
     ## Exercise 2  ##
+
+
 
     # An appropriate phase condition would be to use either derivative of x or y at time 0
     # dxdt(0) or dydt(0) can be used.
+
     def phase_condition(u0, *args): 
         return f(u0, 0,*args)[1] # phase condition
 
-    ## Exercise 3  ##
 
+
+    ## Exercise 3  ##
 
     T = 22 # period estimate
     u0 = [0.4, 0.4] # start conditions estimate
 
 
-    import shooting
     # limit cycle functon taken from Exercise 2
     U0 = (0.4, 0.4, 22)
-    sol = shooting.limit_cycle(f, U0, phase_condition, a, b, d)
+    sol = limit_cycle(f, U0, phase_condition, a, b, d)
     u0 = sol[:-1]
     T = sol[-1]
+
 
     # test orbit against manually found orbit
 
     t_orb = np.linspace(0,T,200)
-    orbitsol = os.solve_ode(f, os.rk4_step, t_orb, u0, h, True, a, b, d)
+    orbitsol = os.solve_ode(f, t_orb, u0, h, True, os.rk4_step, a, b, d)
+
 
     test = np.isclose(orbitsol, manual_orbitsol, atol=1e-03)
     if np.all(test) == True:
